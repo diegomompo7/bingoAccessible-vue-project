e411ec2e41d3f2a0ee8a95b6e562740c0ba3f50a
d.mompo <d.mompo@tvup.media>
2026-05-12 17:09:17 +0200

    detele comments

--- src/components/Bingo.vue
+++ src/components/Bingo.vue
@@ -15,13 +15,6 @@ const props = defineProps<{
 div {
     font-size: 270px;
     //font-size: min(45vw, 80vh);
-    @media (min-width: 320px) { font-size: min(80vw, 120vh);   }  // < 576px  (72px)
-
-    //@include media-breakpoint-up(xs)  { font-size: 4.5rem;   }  // < 576px  (72px)
-    //@include media-breakpoint-up(sm)  { font-size: 8rem;   }  // 576px  (128px)
-    //@include media-breakpoint-up(md)  { font-size: 10.625rem; } // 768px (170px)
-    //@include media-breakpoint-up(lg)  { font-size: 14.25rem; }  // 992px (228px)
-    //@include media-breakpoint-up(xl)  { font-size: 19rem;  }   // 1200px (304px)
-    //@include media-breakpoint-up(xxl) { font-size: 25.25rem; } // 1400px (404px)
+    @media (min-width: 320px) { font-size: min(80vw, 120vh);   }  // < 576px  (72px) 
 }
 </style>
